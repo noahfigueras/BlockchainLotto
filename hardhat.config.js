68f5961dbde85e9adb807313d7a8a0107fa6bf80
noah figueras <noahfigueras@gmail.com>
2021-05-31 14:01:23 -0700

    Fixing test configuration - Error: Invalid ENS names (Working for a fix)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.6.6",
+    solidity: "0.6.6",
+    networks: {
+        hardhat: {
+            forking: {
+                url: process.env.ALCHEMY_URL
+            }
+        }
+    }
 };
--- test/Lottery.js
+++ test/Lottery.js
@@ -2,27 +2,31 @@ const {expect} = require("chai");
 
 describe("Lottery Contract", function() {
 
+    //Defining global variables for testing.
+    const alarm_oracle = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
+    const oraclePayment = 0.1;
+    const price_lottery = 1;
+    const alarm_jobId = "982105d690504c5d9ce374d040c08654";
+    const governance_contract_addr = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
+    let owner;
+    let Lottery;
+    let lottery;
+
     beforeEach(async function () {
-        const [owner] = await ethers.getSigners();
-        const alarm_oracle = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
-        const oraclePayment = 0.1;
-        const price_lottery = 1;
-        const alarm_jobId = "982105d690504c5d9ce374d040c08654";
-        const governance_contract_addr = 0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b;
-    });
+        Lottery = await ethers.getContractFactory("Lottery");
+        [owner] = await ethers.getSigners();
 
-
-    it("is supposed to start a new lottery", async function() {
-
-        const Lottery = await ethers.getContractFactory("Lottery");
-
-        const lottery = await Lottery.deploy(
+        lottery = await Lottery.deploy(
             alarm_oracle,
             oraclePayment,
             price_lottery,
             alarm_jobId,
             governance_contract_addr
         );
+    });
+
+    it("is supposed to start a new lottery", async function() {
+
 
         //Testing ground...
     })
