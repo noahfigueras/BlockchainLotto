ed88bf641716b800294e9882232b37604cfe9907
noah figueras <noahfigueras@gmail.com>
2021-06-16 12:01:06 -0700

    Governance contract testing done

--- test/Governance.js
+++ test/Governance.js
@@ -14,11 +14,42 @@ describe("Governance Contract", function() {
         governance = await Governance.deploy();
     });
 
+    it("reverts when lottery contract address is null", async function () {
+        let state = false;
+
+        try{
+            await governance.lottery()
+        } catch(err) {
+            state = true;
+        }
+
+        expect(state).to.equal(true);
+    })
+
+    it("reverts when RandomConsumerNumber contract address is null", async function () {
+        let state = false;
+
+        try{
+            await governance.randomness();
+        } catch(err) {
+            state = true;
+        }
+
+        expect(state).to.equal(true);
+    })
+
     it("returns lottery contract address", async function () {
         let Lottery = await ethers.getContractFactory("Lottery");
         let lottery = await Lottery.deploy(price_lottery, governance.address);
 
         expect(lottery.address).to.equal(await governance.lottery());
+    });
+
+    it("returns randomNumberConsumer contract address", async function () {
+        let Randomness = await ethers.getContractFactory("RandomNumberConsumer");
+        let randomness = await Randomness.deploy(governance.address);
+
+        expect(randomness.address).to.equal(await governance.randomness());
     });
 
 });
