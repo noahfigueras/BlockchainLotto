223d4fddadebe97613831fcf9156dc7dfde686e9
noah figueras <noahfigueras@gmail.com>
2021-07-03 00:23:44 -0700

    Update Deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,8 +8,16 @@ async function main() {
 
     console.log('Account balance:', (await deployer.getBalance()).toString());
 
+    const vrfCoordinator = "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9";
+
     const Lottery = await ethers.getContractFactory('Lottery');
-    const lottery = await Lottery.deploy(ethers.BigNumber.from(1));
+    const Governance = await ethers.getContractFactory('Governance');
+    const RandomNumberConsumer = await ethers.getContractFactory('RandomNumberConsumer');
+
+    const governance = await Governance.deploy();
+    const lottery = await Lottery.deploy(ethers.utils.parseEther('0.1'), governance.address);
+    const randomNumberConsumer = await RandomNumberConsumer.deploy(governance.address, vrfCoordinator);
+
 
     console.log('Lottery address:', lottery.address);
 }
